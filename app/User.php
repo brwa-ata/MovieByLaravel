fd2bee517bae7df34498e018ce6721df6e8862d7
BrwaAta <brwa.ata.hamalaw@gmail.com>
2017-08-18 01:13:58 -0700

    Create a function to check if user is admin or not

--- app/User.php
+++ app/User.php
@@ -28,4 +28,14 @@ class User extends Authenticatable
     {
         return $this->belongsTo('App\Role');
     }
+
+    public function isAdmin()
+    {
+        if ($this->role->name  == 'admin')
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
