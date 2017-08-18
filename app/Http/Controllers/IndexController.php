a71aa209098604f5e4e031b7e7a8479a19be93e5
BrwaAta <brwa.ata.hamalaw@gmail.com>
2017-08-17 18:51:48 -0700

    Create index Controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EpisodeOfSeason;
+use App\Film;
+use App\SeasonOfTvShow;
+use App\TvShow;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $films = Film::all();
+        $tvshows = TVShow::all();
+
+        return view('startpage.index' , compact('films' , 'tvshows'));
+    }
+
+    public function show($id)
+    {
+        $films = Film::findOrFail($id);
+        $companies = $films->productionCompany;
+        $countries= $films->productionCountry;
+        $genres  = $films->definedGenre;
+        $languages = $films->language;
+
+
+        return view('startpage.show' , compact('tvshows','films','title','companies' , 'countries' , 'genres' , 'languages' ));
+    }
+
+    public function tvshow($id)
+    {
+        $tvshows = TVShow::findOrFail($id);
+        return view('startpage.show_tvshow' , compact('tvshows'));
+    }
+
+    public function season($id)
+    {
+        $tvshows = TVShow::findOrFail($id);
+
+        $seasons = $tvshows->seasons;
+
+         return view('startpage.show_season' , compact( 'seasons'));
+    }
+
+    public function episode($season_id)
+    {
+
+        $seasons = SeasonOfTvShow::findOrFail($season_id);
+        $tvshows = $seasons->tvShow;
+        $episodes = $seasons->episodes;
+
+        return view('startpage.show_episodes' , compact( 'episodes'));
+    }
+
+    public  function episodeInfo($episode_id)
+    {
+       $episodes = EpisodeOfSeason::findOrFail($episode_id);
+       $seasons = $episodes->seasonEpisode;
+        $tvshows = $seasons->tvShow;
+
+        return view('startpage.episode_info' , compact('tvshows' , 'episodes'));
+
+    }
+
+    /**
+     *
+     */
+    public function upcoming()
+    {
+        $now = Carbon::today();
+
+        $films = Film::all()->where('released_date' , ' <= ' , $now);
+
+        echo $films;
+       // echo $films->released_date;
+    }
+
+    public function nowplaying()
+    {
+        $now = Carbon::today();
+        $films = Film::all()->where('released_date' , $now);
+
+        echo $films;
+    }
+}
+
+
+
