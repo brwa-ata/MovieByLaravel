e05377c9ce255f3058c9a792f03205bb919f2acb
BrwaAta <brwa.ata.hamalaw@gmail.com>
2017-08-21 18:05:57 -0700

    Create Film's poster and backdrop form

---
+++ resources/views/admin/film/photo.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.index')
+
+@section('styles')
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.1.1/min/dropzone.min.css" class="css">
+
+@stop
+
+@section('content')
+
+
+    {!! Form::open(['method'=>'POST' , 'action'=>'EpisodePhotoController@store' , 'class' => 'dropzone'])  !!}
+        {{csrf_field()}}
+        <div class="form-group">
+           {!! Form::label('film_id' , 'Choose a Film:') !!}
+           {!! Form::select('film_id' , array() + $films , null , ['class'=>'form-control']) !!}
+        </div>
+
+    {!! Form::close() !!}
+
+@stop
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.js"></script>
